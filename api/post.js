1b23bd8ee6628bb04c516c19c9a33f8b82125f12
Victor Almeida França <fr4ncacc@gmail.com>
2018-10-29 21:51:49 -0300

    alsd

--- api/post.js
+++ api/post.js
@@ -33,7 +33,7 @@ router.get("/", (req, res) => {
 router.post("/add", (req, res) => {
   const { mulher_CPF, descricao, anonimo, dta_post } = req.body;
   console.log(req.body);
-  const qry = `INSERT INTO post VALUES('${dta_post}', '${mulher_CPF}', '${descricao}', ${anonimo})`;
+  const qry = `INSERT INTO post VALUES('${dta_post}', '${mulher_CPF}', '${descricao}', '${anonimo}')`;
   query(qry, err => {
     if (err) res.status(400).send({ error: "Something went wrong" });
     return res.status(200).send({ msg: "Post adicionado" });
